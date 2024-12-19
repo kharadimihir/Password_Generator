e29564a95257ee0fa72dc6facb8034dabe409f89
kharadimihir <kharadimihir739@gmail.com>
2024-12-19 10:09:44 +0530

    Initial commit - Add password generator code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Password_Generator
+# Password Generator
+
+This is a simple password generator built using **React**. The app allows you to generate passwords of varying lengths and complexity by including numbers and special characters.
+
+## Features
+
+- Generate random passwords with customizable length.
+- Option to include numbers and special characters.
+- Clickable checkbox labels for better UX.
+- Ability to copy the generated password to clipboard.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/kharadimihir/Password_Generator.git
---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useCallback, useEffect, useState, useRef } from "react"
+
+function App() {
+
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharaterAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
+
+  const passwordRef = useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str += "0123456789";
+    if(characterAllowed) str += "/!@#$%^&*|";
+
+    for(let i=1; i<=length; i++){
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass)
+    
+  }, [length, numberAllowed, characterAllowed]);
+
+  const copyToClipBoard = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 35)
+    window.navigator.clipboard.writeText(Password)
+  }, [Password])
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, characterAllowed, generatePassword])
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg p-8 mt-64 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center text-4xl mb-8">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          ref={passwordRef}
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0" onClick={()=>{copyToClipBoard( )}}>Copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-2">
+            <input 
+            type="range"
+            min={8}
+            max={35}
+            value={length}
+            onChange={(event) => { setLength(event.target.value)}}
+            className="cursor-pointer"
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            id="number"
+            defaultChecked={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+            />
+            <label htmlFor="number">Number</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            id="character"
+            defaultChecked={characterAllowed}
+            onChange={() => {
+              setCharaterAllowed((prev) => !prev);
+            }}
+            />
+            <label htmlFor="character">Character</label>
+          </div>  
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
